158c57d1cfdf811581700065eb4c2d3fbf3b59d8
Marian Busila <mbusila@pfdrive.com>
2021-12-29 01:31:46 -0500

    add adornment for input text boxes

--- restaurant-app/src/components/Order/OrderForm.js
+++ restaurant-app/src/components/Order/OrderForm.js
@@ -1,27 +1,48 @@
-import { Grid } from "@material-ui/core";
+import { Grid, InputAdornment, makeStyles } from "@material-ui/core";
 import React from "react";
 import { Input, Select, Button } from "../../controls";
 import Form from "../../layouts/Form";
 
 const paymentMethods = [
-    {id: "None", title:"None" },
-    {id: "Cash", title:"Cash" },
-    {id: "Card", title:"Card" },
+  { id: "None", title: "None" },
+  { id: "Cash", title: "Cash" },
+  { id: "Card", title: "Card" },
+];
 
-]
+const useStyles = makeStyles(theme => ({
+    adornmentText: {
+        '& .MuiTypography-root': {
+            color: '#f3b33d',
+            fontWeight: 'bolder',
+            fontSize: '1.5em'
+        }
+    }
+}))
 
 export default function OrderForm(props) {
-    const {values, errors, handleInputChange} = props;
+  const { values, errors, handleInputChange } = props;
+  const classes = useStyles();
+
   return (
     <Form>
       <Grid container>
         <Grid item xs={6}>
-          <Input disabled label="Order Number" name="orderNumber" value={values.orderNumber} />
+          <Input
+            disabled
+            label="Order Number"
+            name="orderNumber"
+            value={values.orderNumber}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment className={classes.adornmentText} position="start">#</InputAdornment>
+              ),
+            }}
+          />
           <Select
             label="Customer"
             name="customerId"
             value={values.customerId}
-            onChange ={handleInputChange}
+            onChange={handleInputChange}
             options={[
               { id: 0, title: "Select" },
               { id: 1, title: "Customer 1" },
@@ -34,10 +55,20 @@ export default function OrderForm(props) {
             label="Payment Method"
             name="paymentMethod"
             value={values.paymentMethod}
-            onChange ={handleInputChange}
+            onChange={handleInputChange}
             options={paymentMethods}
           />
-          <Input disabled label="Total" name="total" value={values.total} />
+          <Input
+            disabled
+            label="Total"
+            name="total"
+            value={values.total}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment className={classes.adornmentText}  position="start">$</InputAdornment>
+              ),
+            }}
+          />
         </Grid>
       </Grid>
     </Form>
